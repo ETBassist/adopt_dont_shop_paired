336cbc7c13d07dff451ed378cc308f993dcb2acc
Eduardo Parra <helloeduardo@icloud.com>
2020-10-09 19:35:13 -0600

    Add seeds'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Shelter.create(
+  name: "Will's Pet Shelter",
+  address: "123 Main St",
+  city: "Boulder",
+  state: "CO",
+  zip: "80309"
+)
+
+Shelter.create(
+  name: "Pet Rescue",
+  address: "10 Normal Rd",
+  city: "Denver",
+  state: "CO",
+  zip: "80249"
+)
+
+Shelter.create(
+  name: "Everywhere Pets!",
+  address: "999 Simple Ln",
+  city: "Fort Collins",
+  state: "CO",
+  zip: "80521"
+)
+
+Shelter.create(
+  name: "Adopt-A-Pet",
+  address: "321 Happy Ave",
+  city: "Aurora",
+  state: "CO",
+  zip: "80011" 
+)
