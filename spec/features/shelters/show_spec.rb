969e492f1f12ca324cb02511f41cbaa76288c855
Eduardo Parra <helloeduardo@icloud.com>
2020-10-10 01:43:28 -0600

    Add edit/update

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -2,22 +2,31 @@ require 'rails_helper'
 
 describe 'As a visitor' do
   describe 'When I visit /shelters/:id' do
-    it "Then I see the shelter's name, address, city, state, and zip" do
-      shelter_1 = Shelter.create!(
-        name: "Will's Pet Shelter",
-        address: "123 Main St",
-        city: "Boulder",
-        state: "CO",
-        zip: "80309"
-      )
+    let(:shelter) { Shelter.create!(
+      name: "Will's Pet Shelter",
+      address: "123 Main St",
+      city: "Boulder",
+      state: "CO",
+      zip: "80309"
+    ) }
 
-      visit "/shelters/#{shelter_1.id}"
+    it "I see the shelter's name, address, city, state, and zip" do
+      visit "/shelters/#{shelter.id}"
 
-      expect(page).to have_content(shelter_1.name)
-      expect(page).to have_content(shelter_1.address)
-      expect(page).to have_content(shelter_1.city)
-      expect(page).to have_content(shelter_1.state)
-      expect(page).to have_content(shelter_1.zip)
+      expect(page).to have_content(shelter.name)
+      expect(page).to have_content(shelter.address)
+      expect(page).to have_content(shelter.city)
+      expect(page).to have_content(shelter.state)
+      expect(page).to have_content(shelter.zip)
+    end
+
+    it 'I can delete the shelter' do
+      visit "/shelters/#{shelter.id}"
+
+      click_on 'Delete Shelter'
+
+      expect(current_path).to eq('/shelters')
+      expect(page).to_not have_content(shelter.name)
     end
   end
 end
