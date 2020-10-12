6bd9f11b7064958474fa776966dcba8e13eeb03b
Eduardo Parra <helloeduardo@icloud.com>
2020-10-12 14:35:46 -0600

    WIP: ActiveRecord filtering

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -3,8 +3,10 @@ class Shelter < ApplicationRecord
 
   def self.sorted_by(param)
     if param == "adoptable"
+      # TODO: Refactor with ActiveRecord
+      # Shelter.joins(:pets).group(:id).order(pets.count(:adoptable))
       Shelter.all.sort_by do |shelter|
-        shelter.pets.count(&:adoptable)
+        shelter.pets.count(:adoptable)
       end.reverse
     elsif param == "alphabetical"
       Shelter.order(:name)
