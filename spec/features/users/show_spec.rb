be19084431789da7f5fceb0904c9a9b82fc0d666
Eugene Theriault <hybridbassist@gmail.com>
2020-10-13 10:32:02 -0700

    Create test for seeing reviews on a users show page

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,22 +2,47 @@ require 'rails_helper'
 
 describe 'As a visitor' do
   describe 'When I visit a Users show page' do
-    let(:user) { User.create!(
-      name: "William Told",
-      street_address: "123 Main St",
-      city: "Boulder",
-      state: "CO",
-      zip: "80309"
-    ) }
+    before :each do
+      @user = User.create!(
+        name: "William Told",
+        street_address: "123 Main St",
+        city: "Boulder",
+        state: "CO",
+        zip: "80309"
+      ) 
+
+      @shelter = Shelter.create!(
+        name: "Will's Pet Shelter",
+        address: "123 Main St",
+        city: "Boulder",
+        state: "CO",
+        zip: "80309"
+      )
+
+      @review = @user.reviews.create!({
+        title: "They are so great!",
+        rating: 5,
+        content: 'I adopted by bun bun through this shelter and they were so great.',
+        shelter_id: @shelter.id
+      } )
+    end
 
     it "I see all that User's information" do
-      visit "/users/#{user.id}"
+      visit "/users/#{@user.id}"
 
-      expect(page).to have_content(user.name)
-      expect(page).to have_content(user.street_address)
-      expect(page).to have_content(user.city)
-      expect(page).to have_content(user.state)
-      expect(page).to have_content(user.zip)
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@user.street_address)
+      expect(page).to have_content(@user.city)
+      expect(page).to have_content(@user.state)
+      expect(page).to have_content(@user.zip)
+    end
+
+    it "I see all the reviews for that User" do
+      visit "/users/#{@user.id}"
+
+      expect(page).to have_content(@review.title)
+      expect(page).to have_content(@review.rating)
+      expect(page).to have_content(@review.content)
     end
   end
 end
