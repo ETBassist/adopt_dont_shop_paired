56f41cc0014b59cbc94769cac83fcd56b12835b6
Eduardo Parra <helloeduardo@icloud.com>
2020-10-13 14:33:34 -0600

    Add user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,10 +8,19 @@ FactoryBot.define do
   end
 
   factory :pet do
-    name { Faker::Superhero.name }
+    name { Faker::Games::Pokemon.name }
     image { "https://placedog.net/280/id=" + rand(1..160).to_s}
     age { rand(1..16).to_s }
-    sex { "Male" }
-    association :shelter
+    sex { ["Male", "Female"].sample }
+    adoptable { [true, false].sample }
+    shelter
+  end
+
+  factory :user do
+    name { Faker::Superhero.name }
+    street_address { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    zip { Faker::Address.zip }
   end
 end
