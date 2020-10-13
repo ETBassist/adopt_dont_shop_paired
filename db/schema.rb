c880777736d18b3438a028ac31429dbb1ad68001
Kate Tester <katemorris@gmail.com>
2020-10-12 18:59:21 -0600

    Adds reviews to setup and reviews to the shelter show page.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_232441) do
+ActiveRecord::Schema.define(version: 2020_10_13_003329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,19 @@ ActiveRecord::Schema.define(version: 2020_10_12_232441) do
     t.datetime "updated_at", null: false
     t.string "description"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.string "content"
+    t.string "image"
+    t.bigint "user_id"
+    t.bigint "shelter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shelter_id"], name: "index_reviews_on_shelter_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "shelters", force: :cascade do |t|
@@ -49,4 +62,6 @@ ActiveRecord::Schema.define(version: 2020_10_12_232441) do
   end
 
   add_foreign_key "pets", "shelters"
+  add_foreign_key "reviews", "shelters"
+  add_foreign_key "reviews", "users"
 end
--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -2,31 +2,58 @@ require 'rails_helper'
 
 describe 'As a visitor' do
   describe 'When I visit /shelters/:id' do
-    let(:shelter) { Shelter.create!(
-      name: "Will's Pet Shelter",
-      address: "123 Main St",
-      city: "Boulder",
-      state: "CO",
-      zip: "80309"
-    ) }
+    before(:each) do
+      @shelter = Shelter.create!(
+        name: "Will's Pet Shelter",
+        address: "123 Main St",
+        city: "Boulder",
+        state: "CO",
+        zip: "80309"
+      )
+
+      @user = User.create!(
+        name: "William Told",
+        street_address: "123 Main St",
+        city: "Boulder",
+        state: "CO",
+        zip: "80309"
+      )
+
+      @review = Review.create!(
+        title: 'They are so great!',
+        rating: 5,
+        content: 'I adopted by bun bun through this shelter and they were so great.',
+        user: @user,
+        shelter: @shelter
+      )
+    end
 
     it "I see the shelter's name, address, city, state, and zip" do
-      visit "/shelters/#{shelter.id}"
+      visit "/shelters/#{@shelter.id}"
 
-      expect(page).to have_content(shelter.name)
-      expect(page).to have_content(shelter.address)
-      expect(page).to have_content(shelter.city)
-      expect(page).to have_content(shelter.state)
-      expect(page).to have_content(shelter.zip)
+      expect(page).to have_content(@shelter.name)
+      expect(page).to have_content(@shelter.address)
+      expect(page).to have_content(@shelter.city)
+      expect(page).to have_content(@shelter.state)
+      expect(page).to have_content(@shelter.zip)
+    end
+
+    it 'I see a list of reviews for that shelter' do
+      visit "/shelters/#{@shelter.id}"
+
+      expect(page).to have_content(@review.title)
+      expect(page).to have_content(@review.rating)
+      expect(page).to have_content(@review.content)
+      expect(page).to have_content(@review.user.name)
     end
 
     it 'I can delete the shelter' do
-      visit "/shelters/#{shelter.id}"
+      visit "/shelters/#{@shelter.id}"
 
       click_on 'Delete Shelter'
 
       expect(current_path).to eq('/shelters')
-      expect(page).to_not have_content(shelter.name)
+      expect(page).to_not have_content(@shelter.name)
     end
   end
 end
