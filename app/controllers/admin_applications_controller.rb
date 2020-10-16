068fc36a0340a42c508a2de23b2a3e5170a9e7aa
Eugene Theriault <hybridbassist@gmail.com>
2020-10-15 18:47:48 -0700

    Create logic to change an Application's status to Approved

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -6,6 +6,11 @@ class AdminApplicationsController < ApplicationController
   def update
     pet_application = PetApplication.find_by(pet_id: params[:pet], application_id: params[:id])
     pet_application.update(status: params[:status])
+    application = Application.find(params[:id])
+    if application.pet_applications.all? { |pet_app| pet_app.status == "approved" }
+      application.status = "Approved"
+      application.save
+    end
     redirect_to "/admin/applications/#{params[:id]}"
   end
 end
