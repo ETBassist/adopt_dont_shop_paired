4f9d9110afaf988cbb28febef3bc02d64cd52f24
Kate Tester <katemorris@gmail.com>
2020-10-18 13:35:59 -0600

    Adds method to update image to default

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,10 @@ class Review < ApplicationRecord
   belongs_to :shelter
 
   validates_presence_of :title, :content, :rating
+
+  def default_image
+    image = "https://cdn.iconscout.com/icon/premium/png-256-thumb/pet-117-805569.png"
+    self.update(image: image)
+    self.save
+  end
 end
