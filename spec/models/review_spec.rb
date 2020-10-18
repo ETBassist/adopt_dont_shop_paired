fddcf1f7f82071c2c8cb46858d95c9f94b66febe
Kate Tester <katemorris@gmail.com>
2020-10-18 14:24:56 -0600

    Adds test for reviews use of parent method sort by

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,6 +5,26 @@ RSpec.describe Review, type: :model do
     it { should validate_presence_of :title }
   end
 
+  describe "class methods" do
+    it '.sorted_by' do
+      @shelter = Shelter.create
+      review1 = create(:review, shelter: @shelter, rating: 1, title: 'WTF')
+      review2 = create(:review, shelter: @shelter, rating: 1, title: 'SRSLY ppl?')
+      review3 = create(:review, shelter: @shelter, rating: 5, title: 'Wowza')
+      review4 = create(:review, shelter: @shelter, rating: 2, title: 'Meh. Sure.')
+      review5 = create(:review, shelter: @shelter, rating: 1, title: 'Run. Fast.')
+      review6 = create(:review, shelter: @shelter, rating: 5, title: 'Love it.')
+
+      highest = [review6, review3, review4, review5, review2, review1]
+      lowest = highest.reverse
+      normal = [review1, review2, review3, review4, review5, review6]
+
+      expect(Review.sorted_by("highest")).to eq(highest)
+      expect(Review.sorted_by("lowest")).to eq(lowest)
+      expect(Review.sorted_by(nil)).to eq(normal)
+    end
+  end
+
   describe "instance methods" do
     it ".default_image" do
       review = create(:review, image: "")
