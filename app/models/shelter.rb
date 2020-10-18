528d3324a5a18110673d8f3933ba1525f28146a2
Kate Tester <katemorris@gmail.com>
2020-10-18 14:17:44 -0600

    Removes sortby mthod to put in parent application record

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,20 +2,6 @@ class Shelter < ApplicationRecord
   has_many :pets, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
-  def self.sorted_by(param)
-    if param == "adoptable"
-      # TODO: Refactor with ActiveRecord
-      # Shelter.joins(:pets).group(:id).order(pets.count(:adoptable))
-      Shelter.all.sort_by do |shelter|
-        shelter.pets.count(:adoptable)
-      end.reverse
-    elsif param == "alphabetical"
-      Shelter.order(:name)
-    else
-      Shelter.all
-    end
-  end
-
   def pet_count
     pets.count
   end
