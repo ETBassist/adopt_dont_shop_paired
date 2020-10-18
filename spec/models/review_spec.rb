430c081a2c1407cb98099cc02a27d0d3fa8d5fef
Kate Tester <katemorris@gmail.com>
2020-10-18 12:43:04 -0600

    Adds test for default image

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Review, type: :model do
   describe "validations" do
     it { should validate_presence_of :title }
   end
+
+  describe "instance methods" do
+    it ".default_image" do
+      review = create(:review, image: "")
+      image = "https://cdn.iconscout.com/icon/premium/png-256-thumb/pet-117-805569.png"
+      correct = review.update(image: image )
+      expect(review.default_image).to eq(correct)
+    end
+  end
 end
