673f1bda15dbbda0be08d409e9599e4d326dda06
Eugene Theriault <hybridbassist@gmail.com>
2020-10-19 18:32:38 -0700

    Change review score to use dropdown rating selection

--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -28,7 +28,8 @@ describe 'As a visitor' do
       expect(current_path).to eq("/reviews/new")
 
       fill_in :title, with: 'They are so great!'
-      fill_in :rating, with: 5
+      #fill_in :rating, with: 5
+      select "5", :from => "rating"
       fill_in :content, with: 'I adopted by bun bun through this shelter and they were so great.'
       fill_in :user_name, with: @user.name
 
@@ -44,7 +45,8 @@ describe 'As a visitor' do
 
       click_link 'New Review'
 
-      fill_in :rating, with: 5
+      #fill_in :rating, with: 5
+      select "5", :from => "rating"
       fill_in :content, with: 'I adopted by bun bun through this shelter and they were so great.'
       fill_in :user_name, with: @user.name
 
@@ -58,7 +60,8 @@ describe 'As a visitor' do
       click_link 'New Review'
 
       fill_in :title, with: 'They are so great!'
-      fill_in :rating, with: 5
+      #fill_in :rating, with: 5
+      select "5", :from => "rating"
       fill_in :content, with: 'I adopted by bun bun through this shelter and they were so great.'
       fill_in :user_name, with: "GobbleGobble"
       click_on 'Create Review'
@@ -69,9 +72,11 @@ describe 'As a visitor' do
       visit "/shelters/#{@shelter.id}"
 
       click_link 'New Review'
+      save_and_open_page
 
       fill_in :title, with: 'They are so great!'
-      fill_in :rating, with: 5
+      #fill_in :rating, with: 5
+      select "5", :from => "rating"
       fill_in :content, with: 'I adopted by bun bun through this shelter and they were so great.'
       fill_in :user_name, with: @user.name
       click_on 'Create Review'
