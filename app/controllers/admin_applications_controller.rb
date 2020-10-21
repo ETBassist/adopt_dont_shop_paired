4ce7c49b0b990d0d3401710298133b6376e84182
Eugene Theriault <hybridbassist@gmail.com>
2020-10-21 12:24:54 -0700

    Update admin application controller with check status method for Application

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -6,12 +6,7 @@ class AdminApplicationsController < ApplicationController
   def update
     pet_application = PetApplication.with_ids(params[:pet], params[:id])
     pet_application.update(status: params[:status])
-    application = Application.find(params[:id])
-    if application.all_pet_apps_approved?
-      application.approve_adoption
-    elsif application.app_rejected?
-      application.reject_adoption
-    end
+    Application.find(params[:id]).check_status
     redirect_to "/admin/applications/#{params[:id]}"
   end
 end
