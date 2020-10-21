18808f6bbc8fcbcca2c58ff20a7527f04e362e7f
Eugene Theriault <hybridbassist@gmail.com>
2020-10-21 13:18:50 -0700

    Add validation of name and image to Pet model

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -56,8 +56,8 @@ describe Pet, type: :model do
   describe 'instance methods' do
     it '.status' do
       shelter = Shelter.create!()
-      pet_1 = shelter.pets.create!()
-      pet_2 = shelter.pets.create!(adoptable: false)
+      pet_1 = create(:pet, shelter: shelter)
+      pet_2 = create(:pet, shelter: shelter, adoptable: false)
 
       expect(pet_1.status).to eq('Adoptable')
       expect(pet_2.status).to eq('Pending Adoption')
