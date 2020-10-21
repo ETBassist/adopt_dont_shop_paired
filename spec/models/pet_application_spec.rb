4899ee1ae7f633e06fa9b537cb4618bfc4f161c9
Eugene Theriault <hybridbassist@gmail.com>
2020-10-21 11:36:09 -0700

    Create test for PetApplication method to find with pet and app id

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe PetApplication, type: :model do
     it { should belong_to :pet }
     it { should belong_to :application }
   end
+
+  describe "model methods" do
+    application = create(:application)
+    pet = create(:pet)
+    pet_app = create(:pet_application, pet: pet, application: application)
+    expect(PetApplication.with_ids(pet.id, application.id)).to eq(pet_app)
+  end
 end
